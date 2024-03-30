608791ec96a46734c6b59659b9fb336304f184a9
manuelarmasme <manu12694@gmail.com>
2024-03-30 19:40:51 -0400

    transaction component done

--- components/trend.vue
+++ components/trend.vue
@@ -5,7 +5,7 @@
     <div class="text-2xl font-extrabold text-black dark:text-white mb-2">
       <USkeleton class="h-8 w-full" v-if="loading" />
 
-      <div v-else>{{ amount }}</div>
+      <div v-else>{{ currency }}</div>
     </div>
 
     <div>
@@ -40,6 +40,9 @@ const icon = computed(() =>
     ? "i-heroicons-arrow-trending-up"
     : "i-heroicons-arrow-trending-down"
 );
+
+//composable
+const {currency} = useCurrency(props.amount)
 
 const percentageTrend = computed(()=> {
   if (props.amount === 0 || props.lastAmount === 0) return '∞%'
